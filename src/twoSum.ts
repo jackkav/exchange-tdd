259432b78fa85bd672410015520bbf75c9d1df5c
Jack Kavanagh <jackkav@gmail.com>
2017-04-16 21:50:26 +0800

    linting

--- src/twoSum.ts
+++ src/twoSum.ts
@@ -1,14 +1,14 @@
 export const twoSum = (nums: number[], target: number): number[] => {
-  if (target == null) return []
-  let result: number[] = []
+  if (target == null) { return []; }
+  const result: number[] = [];
   nums.forEach((element, index) => {
-    if (element === target) result.push(index)
+    if (element === target) { result.push(index); }
     nums.forEach((e, i) => {
       if ((element + e) === target && !result.includes(index) && !result.includes(i)) {
-        result.push(index)
-        result.push(i)
+        result.push(index);
+        result.push(i);
       }
-    })
+    });
   });
-  return result
-}
+  return result;
+};
